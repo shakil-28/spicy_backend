382eb27e796b87f86ca0e5d5ff80bd34ae3a0271
shakil-28 <shakil15-5579@diu.edu.bd>
2025-04-12 07:10:04 +0600

    Update app.py

--- app.py
+++ app.py
@@ -55,5 +55,5 @@ def health_check():
     })
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5012))
     app.run(host='0.0.0.0', port=port)
